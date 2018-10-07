5b6df1c9f0a92a4feb0ec1053592ff0caf553843
Gmacem <33812268+Gmacem@users.noreply.github.com>
2018-10-07 23:58:11 +0500

    shenon
    Add

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var tree = new Array();
-var table = new Array();
-
-function node(inp_name, inp_fr, left, right){
-	this.name = inp_name;
-	this.fr = inp_fr;
-  this.left = left;
-  this.right = right;
-}
-
 function CalcFreq(str){
   var alph = new Array();
   for(i = 0; i < str.length; ++i)
@@ -17,70 +7,16 @@ function CalcFreq(str){
   return alph;
 }
 
-function build_tree(str){
-  let freq = CalcFreq(str);
-  tree = new Array();
-  let id = 0;
-  for (var key in freq){
-    freq[key].id = id++;
-    tree.push(new node(key, freq[key], -1, -1));
-  };
-
-  let n = tree.length;
-  let l = n - 1;
-  let r = n;
-  for (i = 0; i < n - 1; ++i){
-    let firstMin;
-    let secondMin;
-    if (l >= 0 && (r == tree.length || tree[l].fr < tree[r].fr))
-      firstMin = l--;
-    else
-      firstMin = r++;
-    if (l >= 0 && (r == tree.length || tree[l].fr < tree[r].fr))
-      secondMin = l--;
-    else
-      secondMin = r++;
-    tree.push(new node(tree[firstMin].name + tree[secondMin].name, tree[firstMin].fr + tree[secondMin].fr, firstMin, secondMin));
-  }
+function CalcEntropy(freq){
+  let n = freq.Length;
+  let res = 0;
+  freq.forEach(function(item, i, arr) {
+    alert(item + " " + i + " " + arr );
+  })
 }
 
-function build_table(v, cur){
-  if (tree[v].left == -1 && tree[v].right == -1)
-    table[tree[v].name] = cur;
-  else {
-    build_table(tree[v].left, cur + '0');
-    build_table(tree[v].right, cur + '1');
-  }
-}
-
-function code(str){
-  let res = "";
-  for (i = 0; i < str.length; ++i)
-    res += table[str[i]];
-  return res;
-}
-
-function decode(str){
-  let res = "";
-  let root = tree.length - 1;
-  let v = root;
-  for (i = 0; i < str.length; ++i)
-    if (str[i] == '0'){
-      if (tree[v].left == -1){
-        res += tree[v].name;
-        v = tree[root].left;
-      } else {
-        v = tree[v].left;
-      }
-    } else {
-      if (tree[v].right == -1){
-        res += tree[v].name;
-        v = tree[root].right;
-      } else {
-        v = tree[v].right;
-      } 
-    }
-  return res + tree[v].name;
+function logn(val, n) {
+  return Math.log(val) / Math.log(n);
 }
 
 const readline = require('readline');
@@ -90,19 +26,18 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-rl.question('Enter text for build tree: ', (answer) => {
-  build_tree(answer);
-  console.log("Your tree: ");
-  console.log(tree);
-  build_table(tree.length - 1, '');
-  console.log(table);  
-  rl.question('Enter string for encode: ', (answer) => {
-    console.log(code(answer));
-    rl.question('Enter string for decode: ', (answer) => {
-      console.log(decode(answer));
-      rl.close();
-    }); 
-  });
+rl.question('Enter question:\n', (answer) => {
+  let freq = CalcFreq(answer);
+  let result = 0;
+  let size = 0;
+  for (var key in freq){
+    freq[key] /= parseFloat(answer.length);
+    size++;
+  }
+  for (var key in freq) {
+    result -= freq[key] * logn(freq[key], size);
+  }
+  console.log(result);
+  console.log(size);
+  rl.close();
 });
-
